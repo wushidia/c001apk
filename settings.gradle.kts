71d95ce046e1bc42bb7fcc51048b6a5d4cc55e5a
You <308485965@qq.com>
2024-04-08 10:57:59 +0800

    fix: maven aliyun

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
 pluginManagement {
     repositories {
+        mavenLocal()
+        maven (url = "https://maven.aliyun.com/repository/public")
+        maven (url = "https://maven.aliyun.com/repository/gradle-plugin")
+        maven (url = "https://maven.aliyun.com/repository/google")
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -8,6 +12,10 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        mavenLocal()
+        maven (url = "https://maven.aliyun.com/repository/public")
+        maven (url = "https://maven.aliyun.com/repository/gradle-plugin")
+        maven (url = "https://maven.aliyun.com/repository/google")
         google()
         mavenCentral()
         maven ("https://jitpack.io")
