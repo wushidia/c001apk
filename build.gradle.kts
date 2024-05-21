6c6f0b611eb7b8b792b45050fbf8c0c9fc503346
You <308485965@qq.com>
2024-05-21 10:57:48 +0800

    fix: rm ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.google.devtools.ksp) apply false
+    // alias(libs.plugins.google.devtools.ksp) apply false
     alias(libs.plugins.google.dagger.hilt.android) apply false
     alias(libs.plugins.rikka.tools.materialthemebuilder) apply false
     alias(libs.plugins.kotlin.kapt) apply false
